fc292a6319d554e910a1124b9dae75f9916795f7
bmcintosh <bmcintosh@36PLBH2>
2019-07-09 07:09:10 -0400

    validate

--- PasswordValidate/PasswordValidate.js
+++ PasswordValidate/PasswordValidate.js
@@ -1,1 +1,38 @@
 ﻿
+$(function () {
+
+    function validate(password, checkbox, criteria) {
+
+        var okay = false;
+
+        if (criteria.test(password)) {
+            
+            checkbox.removeClass("unchecked").addClass("checked");
+
+            okay = true;
+        }
+        else {
+            checkbox.removeClass("checked").addClass("unchecked");
+        }
+
+        return okay;
+    }
+
+    $('#pass').keyup(function () {
+
+        var thePassword = $('#pass').val();
+
+        validate(thePassword, $("#cap"), /[A-Z]/);
+        validate(thePassword, $("#low"), /[a-z]/);
+        validate(thePassword, $("#num"), /[0-9]/);
+        validate(thePassword, $("#sym"), /[-!@#$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/);
+
+        if (thePassword.length > 7) {
+            $("#len").removeClass("unchecked").addClass("checked");
+        }
+        else {
+            $("#len").removeClass("checked").addClass("unchecked");
+        }
+    });
+
+})
